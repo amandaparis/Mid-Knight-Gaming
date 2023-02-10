542fabc56d67864ce8a68611ebda18de15fa7b0b
ZEN872 <ivanmagana10@hotmail.com>
2023-02-10 10:13:32 -0800

    unity base game

---
+++ Unity_Game_CSE4550/Assets/PlayerMovement.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+        // Start is called before the first frame update
+        private Rigidbody2D player; 
+        private BoxCollider2D coll; 
+        private SpriteRenderer sprite_filp; 
+        private Animator anim;
+
+
+      [SerializeField] private LayerMask jumpable_ground; 
+
+        private enum MovementState{ide, running, jumping, falling}
+        
+
+
+        private void Start()
+        {
+         player = GetComponent<Rigidbody2D>(); 
+         coll = GetComponent<BoxCollider2D>();
+         sprite_filp = GetComponent<SpriteRenderer>() ;
+         anim = GetComponent<Animator>(); 
+        }
+
+    // Update is called once per frame
+        private void Update()
+        {
+          float dirx =  Input.GetAxisRaw("Horizontal");
+          //float diry = Input.GetButtonDown("Vertical");
+
+         player.velocity = new Vector2( 7f * dirx, player.velocity.y);
+
+
+            if(Input.GetButtonDown("Jump") && Isgound() )
+            {
+            
+            player.velocity = new Vector2(player.velocity.x,7f);
+            
+            }
+            
+
+            animations_update( dirx);
+        }
+    
+  private void animations_update(float dirx)
+  {
+      MovementState state; 
+
+            if(dirx >  0f)
+            {
+              //anim.SetBool("Running", true);
+              state = MovementState.running; 
+              sprite_filp.flipX = false;
+            }
+            else if(dirx <  0f)
+            {
+              //anim.SetBool("Running", true);
+              state = MovementState.running; 
+              sprite_filp.flipX = true;   
+
+            }
+            else
+            {
+              //anim.SetBool("Running", false);
+              state = MovementState.ide; 
+            }
+
+            if(player.velocity.y > .1f)
+            {
+              state = MovementState.jumping; 
+            }
+            else if(player.velocity.y < -.1f)
+            {
+              state = MovementState.falling; 
+            }
+
+            anim.SetInteger("state", (int)state ); 
+  }
+
+  private bool Isgound() 
+  {
+    return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, jumpable_ground); 
+  }  
+
+
+}
