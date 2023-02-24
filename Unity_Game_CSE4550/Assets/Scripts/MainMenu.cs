b76911a5fbbb1742e9b060eb992dd4bb0e5b55b0
DiegoR3ys <diego.m.reyes.361999@gmail.com>
2023-02-24 11:58:15 -0800

    Main Menu and Pause Menu
    The Main Menu functions are working as well as the options menu. The dropdown menu bug has been fixed.
    Pause menu has front end done just needs the back end to be programmed in.

---
+++ Unity_Game_CSE4550/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MainMenu : MonoBehaviour
+{
+    /*  
+        The code below is for the Play Button and it pretty much loads the next scene.
+        If you want to change the scene after clicking play go to file on Unity and under the build settings tab you will see the order in which scenes play.
+    */
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    // The code below quits the program when you press quit in the MainMenu.
+    // Debug allows us to see that the Quit button is functioning it obly will work when we build our game and wont work in unity editor.
+    public void QuitGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+}
