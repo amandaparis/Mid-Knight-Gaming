bc78d1bc5f9a2809902ace08eb8aa9844329bb11
NadjaHernandez <nadjahernandez21@gmail.com>
2023-02-23 19:27:30 -0800

    Enemy Skeleton Animations
    Skeleton can spawn from the corpse object, skeleton can walk & patrol an area.

--- Unity_Game_CSE4550/Assets/Enemies/Skeleton/Scripts/Animations.cs
+++ Unity_Game_CSE4550/Assets/Enemies/Skeleton/Scripts/Animations.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Animations : MonoBehaviour
 {
@@ -9,39 +10,142 @@ public class Animations : MonoBehaviour
         private Rigidbody2D Enemy; 
         private BoxCollider2D coll; 
         private Animator anim;
+    private SpriteRenderer rend;
+        PlayerMovement player;
+        
         public GameObject head;
         public GameObject deadParticle;
       
         public int maxHP = 200; 
         int currentHp; 
 
-      // private enum MovementState{Idle, Death}
+    public enum MovementState{Idle, Death, Walk, Attack, Reborn}
+    public MovementState currentState = MovementState.Reborn;
+
+    //direction of the skeleton moving
+    public int direction;
+    public float distance;      //distance from skeleton to player
+    public float attackRange;   //range that the player needs to be for enemy to attack
+    [SerializeField]
+    float walkSpeed;            //walk speed of the enemy
+
+    public float timeBetweenAttacks;    //time that has to happen for enemy to attack again
+    public float attackCools;           //
+
+    public bool hurt;                   //if enemy is hurt, for hurt animation
+    public bool attacking = false;      //if enemy is attacking, for attacking animation
         
         private void Start()
         {
          Enemy = GetComponent<Rigidbody2D>(); 
          coll = GetComponent<BoxCollider2D>();
          anim = GetComponent<Animator>();
-
-          currentHp = maxHP; 
-
-        }
+        rend = GetComponent<SpriteRenderer>();
+         currentHp = maxHP;
+        hurt = false;
+        player = FindObjectOfType<PlayerMovement>();      //makes the player's functions avail to this script
+        anim.SetBool("walk", true);                       //enemy will start walking as soon as it is spawned
+    }
 
     // Update is called once per frame
-        private void Update()
+    private void Update()
+    {
+        //walking function
+        if (isFacingRight())
         {
-          
-
-         
+            Enemy.velocity = new Vector2(walkSpeed, 0f);
+        }
+        else
+        {
+            Enemy.velocity = new Vector2(-walkSpeed, 0f);
         }
 
+        //distance from enemy to player
+        distance = Vector2.Distance(transform.position, player.transform.position);
+        //if distance is within attackrange then it will start to attack
+        if (distance <= attackRange)
+            currentState = MovementState.Attack;
+        else
+            currentState = MovementState.Walk;
+/*
+        rend.flipX = (direction == -1);
+            switch (currentState)
+        {
+            case MovementState.Death:
+                Death();
+                break;
+            case MovementState.Reborn:
+                Reborn();
+                break;
+            case MovementState.Walk:
+                Walk();
+                break;
+            case MovementState.Idle:
+                Idle();
+                break;
+            case MovementState.Attack:
+                Attack();
+                break;
+        }
+*/
+        if (attackCools > 0) attackCools -= Time.deltaTime;
+    }
 
-public void takedamage(int damage)
+    //is enemy facing right?
+    private bool isFacingRight()
+    {
+        return transform.localScale.x > Mathf.Epsilon;
+    }
+
+    //function for enemy to ignore the player object & not get stuck on it
+    //will add more if statements if there are more objects*********
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (!collision.gameObject.CompareTag("Player"))
+        {
+            transform.localScale = new Vector2(-(Mathf.Sign(Enemy.velocity.x)), transform.localScale.y);
+        }
+        
+    }
+
+    /// <summary>
+    /// These two functions will be completed with player's hurt functions
+    /// </summary>
+    /// <param name="collision"></param>
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            if(attacking == true)
+            {
+
+            }
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            if (attacking == true)
+            {
+
+            }
+        }
+    }
+
+    /// <summary>
+    /// End of TODO
+    /// </summary>
+    /// <param name="damage"></param>
+    /// 
+
+    //Take damage function that will be used by player scripts***********
+    public void takedamage(int damage)
 {
   currentHp = currentHp - damage;
 
-    //anim.SetTrigger("hit");
-
+        //anim.SetTrigger("hit");
 
 
     Debug.Log("Enemy HP : " + damage);
@@ -51,16 +155,23 @@ public void takedamage(int damage)
             Death();
             activeParticles(); 
      }
-     else
-     {
-      anim.SetTrigger("hit");
-     }
+     
 }
 
+    public void Hurt()
+    {
+        anim.SetBool("hurt", true);
+        Invoke("ResetHurt", 0.1f);
+    }
 
+    void ResetHurt()
+    {
+        anim.SetBool("hurt", false);
+        hurt = false;
+    }
 
-//Activates on keyframe
-private void Death()
+    //Activates on keyframe
+    private void Death()
     {
         
         //enable the head, turn off physics, and turn off collisions
@@ -82,4 +193,24 @@ private void Death()
     anim.SetTrigger("Skeleton_death");
 
   }
+
+    private void Attack()
+    {
+        if (attackCools < 0)
+        {
+            anim.SetBool("attack", true);
+            Invoke("ResetAttack", 0.1f);
+            attackCools = timeBetweenAttacks;
+        }
+        else
+            currentState = MovementState.Walk;
+    }
+
+    void ResetAttack()
+    {
+        anim.SetBool("attack", false);
+    }
+
+
+
 }
