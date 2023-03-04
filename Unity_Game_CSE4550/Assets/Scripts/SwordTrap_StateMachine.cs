888f95da0cf46aaa42c99ae4d3a81d70b77d66c4
Kevin Inz <45952314+KevinInzu@users.noreply.github.com>
2023-03-03 17:46:30 -0800

    Updated SwordTrap distance based
    Sword trap now activates based on distance rather than trigger

--- Unity_Game_CSE4550/Assets/Scripts/SwordTrap_StateMachine.cs
+++ Unity_Game_CSE4550/Assets/Scripts/SwordTrap_StateMachine.cs
@@ -15,37 +15,68 @@ public struct TrapProperties
 public class SwordTrap_StateMachine : MonoBehaviour
 {
     [SerializeField] public float Damage;
+    [SerializeField] float attack_distance;
 
-    [Header("Timers")]
+    [Header("Timer")]
     [SerializeField] public float activateTime;
 
     [HideInInspector] public TrapProperties trapProperties;
+    private float distance;
+    PlayerActions player;
+
     private string Sword_State;
     // Start is called before the first frame update
     void Start()
     {
+        player = FindObjectOfType<PlayerActions>();
         trapProperties.arm = transform.Find("Arm");  //Get references from child
         trapProperties.anim = trapProperties.arm.GetComponent<Animator>();
         trapProperties.spriteRend = trapProperties.arm.GetComponent<SpriteRenderer>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        distance = Vector2.Distance(transform.position, player.transform.position);
+        
+        ///////////////////////////////////////////////////
+        ////////// Controller
+        ///////////////////////////////////////////////////
+
+        /* 
+        Check the distance for trap to attack is close to the player, and
+        the trap hasn't been triggered
+        */
+        if (distance <= attack_distance && !trapProperties.triggered)
+        {
+            Sword_State = "Triggered";
+        }
+        /* 
+        If the player is out of distance , or trigger is already on 
+        then just turn it off (prevents spamming) 
+        */
+        else
+        {
+            trapProperties.triggered = false;
+        }
+
         ///////////////////////////////////////////////////
         ////////// State Machines
         ///////////////////////////////////////////////////
+
         switch (Sword_State)
         {
             case "Triggered":
-                if (!trapProperties.triggered && !Input.GetButton("DUCKING")) //Check if it hasn't been triggered yet, to avoid spamming it, and not ducking
+                if (!trapProperties.triggered) //Check if it hasn't been triggered yet, to avoid spamming it, and not ducking
                 {
                     trapProperties.triggered = true;
                     trapProperties.active = true;
                     trapProperties.anim.SetBool("Slash", true);
                     StartCoroutine(ActivateTrap());
                 }
-                if(trapProperties.active) //If the trap is already triggered, then it's active
+                if (trapProperties.active) //If the trap is already triggered, then it's active
                 {
                     Sword_State = "Active";
                 }
@@ -56,26 +87,20 @@ public class SwordTrap_StateMachine : MonoBehaviour
                 trapProperties.triggered = false;
                 trapProperties.anim.SetBool("Slash", false);
                 break;
-                
+
             case "Active":
                 //TODO: Take damage
                 Sword_State = "Triggered";
-            break;
+                break;
+                
             default:
                 break;
         }
 
     }
     ///////////////////////////////////////////////////
-    ////////// Controllers
+    ////////// Controller
     ///////////////////////////////////////////////////
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.tag == "Player")
-        {
-            Sword_State = "Triggered";
-        }
-    }
 
     public IEnumerator ActivateTrap()
     {
