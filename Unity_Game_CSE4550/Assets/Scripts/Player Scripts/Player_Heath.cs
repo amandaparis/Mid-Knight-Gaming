51250d0922ccc0032b58f5c40dc77147c90fcf9c
ZEN872 <ivanmagana10@hotmail.com>
2023-03-04 15:51:42 -0800

    hearts sprite fixes

--- Unity_Game_CSE4550/Assets/Scripts/Player Scripts/Player_Heath.cs
+++ Unity_Game_CSE4550/Assets/Scripts/Player Scripts/Player_Heath.cs
@@ -28,8 +28,24 @@ public class Player_Heath : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+            if(health > numofhearts)
+            {
+                health = numofhearts;
+            }
+
         for(int i =0; i < hearts.Length; i++)
         {
+            if(i < health)
+            {
+                hearts[i].sprite = fullHearts;
+            }
+            else
+            {
+                hearts[i].sprite = emptyHearts;
+            }
+
+
             if(i < numofhearts)
             {   
                 hearts[i].enabled =true; 
