9e9d97c16805d440da267043743cba2354cdf52b
ZEN872 <119709289+ZEN872@users.noreply.github.com>
2023-04-24 09:59:43 -0700

    skeleton fixes

---
+++ Unity_Game_CSE4550/Assets/Scripts/Skeletons/GIANT_SKELE/Gskele.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gskele : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+
+    private void OnCollisionEnter2D(Collision2D collision) //Player_Attack_point
+    {
+        Debug.Log("Giant_SKELETON_COLLIDE");
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+           GetComponent<enemy_class>().Enemy_take_damage(100);
+           Debug.Log("ENEMY HAS DIED");
+        }
+
+        if(collision.gameObject.CompareTag("Player"))
+        {
+             GetComponent<Player_Heath>().player_takeDamage(100);
+              Debug.Log("PLAYER HAS DIED");
+        }
+    }
+
+
+
+
+}
