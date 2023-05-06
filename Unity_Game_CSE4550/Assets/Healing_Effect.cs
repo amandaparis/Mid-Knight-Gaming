28dc72f6a59c8a624c3e057611e6d9a8a9f0cf18
Kevin Inz <45952314+KevinInzu@users.noreply.github.com>
2023-05-05 19:39:54 -0700

    Healing and Chest Death sound

--- Unity_Game_CSE4550/Assets/Healing_Effect.cs
+++ Unity_Game_CSE4550/Assets/Healing_Effect.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Healing_Effect : MonoBehaviour
 {
+    private bool sound_played = false;
+    [SerializeField] private AudioSource sound;
     public float cleanup_time;
     // Start is called before the first frame update
     void Start()
@@ -16,7 +18,12 @@ public class Healing_Effect : MonoBehaviour
     {
         Debug.Log(gameObject.transform.position);
         gameObject.transform.localPosition = new Vector3(0, -0.05f, 0);
-        gameObject.transform.localScale = new Vector3(1,1,1);
+        gameObject.transform.localScale = new Vector3(1, 1, 1);
+        if (!sound_played)
+        {
+            sound.Play();
+            sound_played = true;
+        }
         StartCoroutine(cleanup());
     }
 
