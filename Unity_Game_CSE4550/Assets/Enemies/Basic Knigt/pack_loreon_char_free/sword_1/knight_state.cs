6bee2804a4c4cd06a25c31bfc408e74bfdef6f59
ZEN872 <ivanmagana10@hotmail.com>
2023-05-07 15:35:52 -0700

    fg

--- Unity_Game_CSE4550/Assets/Enemies/Basic Knigt/pack_loreon_char_free/sword_1/knight_state.cs
+++ Unity_Game_CSE4550/Assets/Enemies/Basic Knigt/pack_loreon_char_free/sword_1/knight_state.cs
@@ -4,20 +4,168 @@ using UnityEngine;
 
 public class knight_state : basic_skele_class
 {
+public Animator anim;
 
-    
+    public string CurrentState ;
+
+    public bool awaking = true;   
+    float awakening_delay = 0f; 
 
 
-
+    float ATT_delay = 0f; 
+    float STUN_delay = 1f; 
+    float stun_time =0; 
     // Start is called before the first frame update
+    public bool right_facing = false; 
+
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
+        if(awaking)
+        {
+            CurrentState = "base"; 
+            anim.SetInteger("state", 0 );  
+        }
+        else
+        {
+            CurrentState = "IDE"; 
+            anim.SetInteger("state", 1 ); 
+        }
+
+        if(right_facing) 
+        {
+            sprite_filp.flipX = false;
+        }
+        else 
+        {
+             sprite_filp.flipX = true;
+        }
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        if( CurrentState != "base" &&  CurrentState !="awaking")
+            CurrentState = checkHP(CurrentState); 
+        else if(CurrentState != "HURT" && CurrentState != "IDE")
+        {
+            enemy_stop();
+            stun_time = Time.time + STUN_delay; 
+        }
+
+        if(CurrentState == "DEATH")
+        {
+            stun_time = Time.time + STUN_delay; 
+        }
+
+
+        switch(CurrentState) 
+        {
+            /////////////////////////////////////////////////
+            case "base": // BONE state 0 
+            //////////////////////////////////////////////////
+                {
+                    anim.SetInteger("state", 0 ); 
+                    if(skel_awake())
+                    {
+                         Debug.Log("BASE");
+                        awakening_delay = Time.time + 1f  ;  
+                        CurrentState = "IDE"; 
+                         anim.SetInteger("state", 1);
+                    }
+                }
+                break;
+            /////////////////////////////////////////////////////
+            case "IDE": // IDE STATE 2
+            ///////////////////////////////////////////////////////
+                {
+                    anim.SetInteger("state", 0); 
+                    
+                    if(stun_time <= Time.time )
+                    {
+                        if(trigger_attack())
+                        {
+                            ATT_delay  = Time.time + 0.5f;
+                            //Debug.Log(ATT_delay);
+                            CurrentState = "ATT";
+                        }
+                        else 
+                        {
+                            CurrentState = "WALK";
+                        }
+                    }
+                }
+                break;
+            ///////////////////////////////////////////////////////
+            case "ATT": // att STATE // 3 
+            ///////////////////////////////////////////////////////
+                anim.SetInteger("state", 1 ); 
+                //Debug.Log("ATT");
+                if(ATT_delay <= Time.time)
+                {
+                    damage_player();
+                    CurrentState ="IDE";
+                     stun_time = Time.time + STUN_delay; 
+                }
+             break;
+              ///////////////////////////////////////////////////////
+            case "WALK": // att STATE // 4 
+            ///////////////////////////////////////////////////////
+            anim.SetInteger("state", 2); 
+                if(Enemy.position.x > max_x)
+                {
+                    sprite_filp.flipX = true;
+                }
+                else if(Enemy.position.x < min_x)
+                {
+                    sprite_filp.flipX = false;    
+                }
+                walk(); 
+
+                if(trigger_attack())
+                {
+                    ATT_delay  = Time.time + 0.5f;
+                        CurrentState = "ATT"; 
+                }
+             break;
+            ///////////////////////////////////////////////////////
+             case "HURT": // Hurt STATE // 5 
+            /////////////////////////////////////////////////////// 
+                //enemy_stop();
+                  //anim.SetInteger("state", 5); 
+                if(stun_time <= Time.time )
+                {
+                    stun_time = Time.time + STUN_delay; 
+                    //Debug.Log("Hurt_skeleton");
+                    CurrentState = "IDE";
+
+                    if(GetComponent<enemy_class>().CurrentHp() <= 0)
+                    {
+                         anim.SetTrigger("death");
+                        CurrentState = "DEATH";
+                    } 
+                }
+                break; 
+            /////////////////////////////////////////////////////
+            case "DEATH":
+            ////////////////////////////////////////////////////
+             //anim.SetTrigger("death");
+             Enemy.bodyType = RigidbodyType2D.Static;
+                if(stun_time <= Time.time )
+                {
+                gameObject.GetComponent<SpriteRenderer>().enabled = false;
+                }
+                break;
+            /////////////////////////////////////////////////////
+            default:
+            if(stun_time <= Time.time )
+                {
+                Debug.Log("Unknown Action"); 
+                }
+                break;
+        }
     }
 }
